e3d75c061864b9e34b71b662fbfe2d1a2039195d
Luis Ramirez <luis@liquimind.com>
2015-01-17 18:25:39 +0800

    Added automated nuget creation.

--- BluetoothBear/Properties/AssemblyInfo.cs
+++ BluetoothBear/Properties/AssemblyInfo.cs
@@ -29,9 +29,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BluetoothBear")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bluetooth Library for Xamarin.Forms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Liquimind Inc.")]
 [assembly: AssemblyProduct("BluetoothBear")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
--- BluetoothBearAndroid/Properties/AssemblyInfo.cs
+++ BluetoothBearAndroid/Properties/AssemblyInfo.cs
@@ -28,10 +28,10 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BluetoothBearAndroid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Android Bluetooth Support Library for BluetoothBear")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BluetoothBearAndroid")]
+[assembly: AssemblyCompany("Liquimind Inc.")]
+[assembly: AssemblyProduct("BluetoothBear_iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -47,5 +47,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
